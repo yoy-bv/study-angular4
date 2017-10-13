66aa6c69d71c70412bb87288caba303889b4fcc1
yoy-bv <vuongnguyenthanhbinh@gmail.com>
2017-10-13 14:20:10 +0700

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,35 +9,7 @@ import { Hero } from './hero';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  text = 'Welcome Text';
-  image='http://lorempixel.com/300/300';
-  applyClass = true;
-  applyColor = true;
-  heroes = HEROES;
-  myHero = this.heroes[0];
-
-  // click me
-  public clickbinding:any;
-  OnClick(value){
-    this.clickbinding = value;
-  }
-  public agree:number = 0;
-  public disgree:number = 0;
-
-  pesions = ['Mr A', 'Mr B','Mr C','Mr D']
-  parentVote(agree:boolean){
-    if (agree) this.agree ++;
-    else this.disgree ++;
-  }
+  today = Date.now();
+  percent = 1.235345;
+  opject:Object = {abc:'abc', cdf:'cdf', number:['1','2','3','4']};
 }
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-];
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Hero } from './hero';
@@ -7,14 +7,4 @@ import { Hero } from './hero';
   templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
-  @Input() name:string;
-  @Output() onvote = new EventEmitter<boolean>();
-
-  public voted:boolean = false;
-  public votee:boolean;
-  vote(agree:boolean){
-    this.votee = agree;
-    this.voted = true;
-    this.onvote.emit(agree);
-  }
 }
